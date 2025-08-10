0f3cd88345e5d8e4852eb5597b5780e24d1ff79d
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-08-11 00:20:55 +0530

    feat: add custom logger

--- 003_hono-prisma-mongo/src/config/env.config.ts
+++ 003_hono-prisma-mongo/src/config/env.config.ts
@@ -1,5 +1,7 @@
-const PORT = parseInt(String(process.env.PORT)) || 3030;
+// radix 10 to parse the string into int in decimal system (0 to 9)
+const PORT = parseInt(String(process.env.PORT), 10) || 3030;
 
 export const config = {
-  PORT
+  PORT,
+  APP_ENV: process.env.APP_ENV || process.env.NODE_ENV || "development"
 }
